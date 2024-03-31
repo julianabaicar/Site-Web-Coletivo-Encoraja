d7413398d3176f76075a460ad6dc3a3c0e74ebfb
André Lucas Baldaia <andrelucasbaldaia@gmail.com>
2024-03-30 22:58:04 -0300

    Criado cenarios de teste

--- tests/address_test.php
+++ tests/address_test.php
@@ -1,23 +1,34 @@
 <?php
-
-require_once 'User.php';
-require_once '../tests/Address.php';
+require_once '../class/Voluntary.php';
+require_once '../class/Address.php';
 
 $voluntario = array();
 
-$voluntario[0] = new Voluntary(123456789, 123456, "Andre", "andre@example.com", "1990-01-01", "Voluntario", true, true, true, 'Finais de semana', 'Bastante', 'Amigos', 'altas');
-$voluntario[1] = new Voluntary(123456789, 123456, "Maria", "maria@example.com", "1990-01-01", "Voluntario", true, true, true, 'Finais de semana', 'Bastante', 'Amigos', 'altas');
-$voluntario[2] = new Voluntary(123456789, 123456, "Juliana", "juliana@example.com", "1990-01-01", "Voluntario", true, true, true, 'Finais de semana', 'Bastante', 'Amigos', 'altas');
+$voluntario[0] = new Voluntary(123456789, "Andre", 123456, "1990-01-01", 123456, "Voluntario", true, true, true, 'Finais de semana', 'Bastante', 'Amigos', 'altas');
+$voluntario[1] = new Voluntary(123456789, "Maria", 123456, "1990-01-01", 123456, "Voluntario", true, true, true, 'Finais de semana', 'Bastante', 'Amigos', 'altas');
+$voluntario[2] = new Voluntary(123456789, "Juliana", 123456, "1990-01-01", 123456, "Voluntario", true, true, true, 'Finais de semana', 'Bastante', 'Amigos', 'altas');
 
+//Cenario 01: Criar endereço
 $endereco = array();
 $endereco[0] = new Address();
 $endereco[1] = new Address();
 $endereco[2] = new Address();
 
-$endereco[0]->registrerAddress("Rua Eva Pereira Correia", 133, "Sitio Cercado", "Curitiba", 81910045, $voluntario[0]);
-$endereco[1]->registrerAddress("Rua das Graças", 133, "Portão", "Curitiba", 81910045, $voluntario[1]);
-$endereco[2]->registrerAddress("Rua Derosso", 133, "Boqueirão", "Curitiba", 81910045, $voluntario[2]);
+$endereco[0]->registerAddress("Rua Eva Pereira Correia", 133, "Sitio Cercado", "Curitiba", 81910045, $voluntario[0]);
+$endereco[1]->registerAddress("Rua das Graças", 133, "Portão", "Curitiba", 81910045, $voluntario[1]);
+$endereco[2]->registerAddress("Rua Derosso", 133, "Boqueirão", "Curitiba", 81910045, $voluntario[2]);
 
+if($endereco){
+    print_r($endereco);
+    echo 'Sucesso! Teste Cenario 01' .PHP_EOL;
+} else {
+    echo  'Erro: teste Cenario 01' .PHP_EOL;
+}
+
+//Cenario 02: Editar endereço
 $endereco[0]->changeAddress("Rua Derosso", 133, "Boqueirão", "Curitiba", 81910045, $voluntario[0]);
-
 print_r($endereco[0]);
+
+echo 'Endereço alterado com sucesso';
+
+
